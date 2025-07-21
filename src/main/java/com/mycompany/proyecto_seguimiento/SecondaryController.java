ebda0f7e97ce088e353c8491fbe5fa31dedd9264
natsof1989 <nathalia1989nath@gmail.com>
2025-07-21 10:06:28 -0400

    controller

--- src/main/java/com/mycompany/proyecto_seguimiento/SecondaryController.java
+++ src/main/java/com/mycompany/proyecto_seguimiento/SecondaryController.java
@@ -1,12 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
 package com.mycompany.proyecto_seguimiento;
 
-import java.io.IOException;
-import javafx.fxml.FXML;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
 
-public class SecondaryController {
+/**
+ * FXML Controller class
+ *
+ * @author natha
+ */
+public class SecondaryController implements Initializable {
 
-    @FXML
-    private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
-    }
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
 }
