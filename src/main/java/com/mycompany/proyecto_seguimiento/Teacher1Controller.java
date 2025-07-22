b0cc9d36dfe27efbcfd052d7b3dd766be2aa23a0
natsof1989 <nathalia1989nath@gmail.com>
2025-07-22 07:29:46 -0400

    actualizacion

---
+++ src/main/java/com/mycompany/proyecto_seguimiento/Teacher1Controller.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompany.proyecto_seguimiento;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author natha
+ */
+public class Teacher1Controller implements Initializable {
+
+    @FXML
+    private Label txt_bienvenida;
+    @FXML
+    private Label txt_nombre;
+    @FXML
+    private Label txt_especialidad;
+    @FXML
+    private Label txt_ci;
+    @FXML
+    private Button bt_verCaso;
+    @FXML
+    private Button bt_casoNew;
+    @FXML
+    private Button bt_orientaciones;
+    @FXML
+    private Button bt_configurar;
+    @FXML
+    private Button bt_cerrarSesion;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
