262db44e3c0662d76ed6d59a74e3ff8e55029269
natsof1989 <nathalia1989nath@gmail.com>
2025-07-28 16:44:08 -0400

    actualizacion

---
+++ src/main/java/com/mycompany/proyecto_seguimiento/modelo/Usuario.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto_seguimiento.modelo;
+
+/**
+ *
+ * @author natha
+ */
+public class Usuario {
+    private int cedula; 
+    private String nombre; 
+    private String apellido; 
+    private String especialidad; 
+    private int id_departamento; 
+    private String espe; 
+    
+}
